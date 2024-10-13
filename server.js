1f9224669adc396afbb7114b22341919fdfe582c
Youssef <user@Air-von-User.fritz.box>
2024-10-13 23:37:46 +0200

    fixing port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const authRoutes = require('./routes/authRoutes');
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 require('dotenv').config();
 
