bde2a2fe897d08d9fe554ebc6659be685cf6b51e
Youssef <user@Air-von-User.fritz.box>
2024-10-13 23:34:11 +0200

    fixing server.js, adding the missing endpoints

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const authRoutes = require('./routes/authRoutes');
+const PORT = process.env.PORT || 5001;
+
 require('dotenv').config();
 
 app.use(express.json());
 
+app.use('/api/auth', authRoutes);
 app.get('/', (req, res) => {
   res.send('Studentify Backend Running');
 });
